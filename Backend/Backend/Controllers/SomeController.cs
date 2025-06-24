83b97e52d22072de21985f970b6d7ee779219676
Fernando VI. <f3rnandogaff@gmail.com>
2025-06-23 23:29:33 -0600

    Programación síncrona y asíncrona

---
+++ Backend/Backend/Controllers/SomeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+
+        //Creación de un método síncrono
+        [HttpGet("sync")]
+
+        public IActionResult GetSync()
+        {
+            //Añadimos un cronómetro para medir el tiempo de ejecución
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            // Simulación de una operación que toma tiempo
+            Thread.Sleep(2000);
+            Console.WriteLine("Operación a BD terminada...");
+
+            Thread.Sleep(2000);
+            Console.WriteLine("Envío de mail terminado...");
+
+
+            Console.WriteLine("Todo ha terminado correctamente");
+            // Detenemos el cronómetro
+            stopwatch.Stop();
+            return Ok(stopwatch.Elapsed);
+        }
+
+        [HttpGet("async")]
+        //Solo regresará una respuesta HTTP cuando se complete la tarea asíncrona
+        public async Task<IActionResult> GetAsync()
+        { 
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            var task1 = new Task<int>(() =>
+            {
+                // Simulación de una operación que toma tiempo
+                Thread.Sleep(1000);
+                Console.WriteLine("Operación a BD terminada...");
+                return 11;
+            });
+
+            var task2 = new Task<int>(() =>
+            {
+                // Simulación de una operación que toma tiempo
+                Thread.Sleep(1000);
+                Console.WriteLine("Envío de mail terminado...");
+                return 12;
+            });
+
+            task1.Start();
+            task2.Start();
+
+            Console.WriteLine("Aquí se hace otra tarea...");
+
+            //Verificamos si las tareas han terminado para retornarlo
+           var result1 = await task1;
+           var result2 = await task2;
+
+            Console.WriteLine("Todo ha terminado...");
+
+            stopwatch.Stop();
+
+            return Ok(result1 + " " + result2 + " " + stopwatch.Elapsed);
+
+        }
+    }
+}
--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82008cf17793f6c2f91fe44f2ef9ab2837a0897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dad8423843c6434f2d48e073c6bb15241fe4dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
